6a4e359e2e0cfdff7e99585acfd34e918eb22449
v0lp3 <andreamaujeri@gmail.com>
2021-05-01 19:27:11 +0200

    feat(sploiter): add send to server

--- sploiter.py
+++ sploiter.py
@@ -1,5 +1,5 @@
 from subprocess import check_output
-from utils import get_flag, get_teams, get_config
+from utils import parse_flag, get_teams, get_config
 from concurrent.futures import ThreadPoolExecutor
 import threading
 
@@ -14,22 +14,34 @@ class Exploit:
         self.executor = ThreadPoolExecutor(
             max_workers=get_config()["client"]["max_thread"])
 
-    def store_flag():
+    def store_flag(self, team_name, flag):
         with self._lock:
             entry = (team_name, flag)
 
             if entry not in self.flags:
-                flags.append(entry)
+                self.flags.append(entry)
+
+            
 
     def attack_team(self, team_name, ip_address):
 
         script_path = f'{get_config()["client"]["exploit_path"]}/{self.name}'
         output = check_output(["python3", script_path, ip_address]).decode()
 
-        flag = get_flag(output)
+        flag = parse_flag(output)
 
         if flag is not None:
-            store_flag(flag)
+            self.store_flag(team_name, flag)
+
+    def get_flags(self):
+
+        with self._lock:
+            data = [(team_name, self.name, flag)
+                    for team_name, flag in self.flags]
+
+            self.flags.clear()
+
+        return data
 
     def run_exploit(self):  # run script for every team
 
@@ -37,3 +49,4 @@ class Exploit:
 
         for team_name in teams:
             self.executor.submit(self.attack_team, team_name, teams[team_name])
+
